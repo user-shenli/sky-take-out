08c7c1640ea450e8d04004e484437cc6d8aab930
user-shenli <2871353065@qq.com>
2024-11-16 20:59:43 +0800

    编辑员工信息

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -29,4 +29,18 @@ public interface EmployeeService {
     * 启用禁用员工账号
     * */
     void startOrStop(Integer status, Long id);
+
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    Employee getById(Long id);
+
+    /**
+     * 编辑员工信息
+     * @param employeeDTO
+     */
+    void update(EmployeeDTO employeeDTO);
+
 }
